6c1279eb6fefe6889d1af45489d09f709f974c18
melrwa <orwamelkizedek@gmail.com>
2024-09-18 14:56:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name = "Aki") {
+    return `Hi, my name is ${name}.`;
+  }
+  
+  console.log(introduction());
+  
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  console.log(introductionWithLanguage("Ivy", "Python"));
+  
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  console.log(introductionWithLanguageOptional("Gracie"));
+  console.log(introductionWithLanguageOptional("Gracie", "Ruby"));
+  
